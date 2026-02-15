c3332c6e62d6e09310202c3ec0f63c956287532e
CamilaRaimundo <camilaapereira.r@gmail.com>
2026-02-15 12:12:45 -0300

    Exercício 1 - Criação de API's do tipo GET

---
+++ Ex1.py
@@ -0,0 +1,50 @@
+# Crie uma API utilizando Flask/FastAPI com os seguintes endpoints:
+    # GET / → retorna { "message": "API funcionando" }
+    # GET /status → retorna: { "status": "online", "version": "1.0" }
+
+from flask import Flask, request, jsonify
+
+# Flask → cria a aplicação
+# request → acessa dados da requisição (query params, JSON, etc.)
+# jsonify → transforma dicionários Python em JSON
+
+
+# cria a aplicação Flask (instância)
+app = Flask(__name__)
+
+# Endpoints são os caminhos da URL que sua API disponibiliza. Por padrão são GET
+@app.route('/funcionando')
+def index():
+    dados = {'mensagem': 'API funcionando'} #isso é um dicionário Python
+    return jsonify(dados), 200, {'Content-Type': 'application/json; charset=utf-8'} #transforma em JSON e o retorna no endpoint
+
+@app.route('/infos', methods=['GET']) #pode ser adicionado o método
+def informacoes():
+    info = {'status': 'online', 'version': '1.0'}
+    return jsonify(info), 200, {'Content-Type': 'application/json; chatset=utf-8'}
+
+
+# Ela sobe um servidor local em: http://localhost:5000
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# -----------------------------------------------------------------------------------------------
+# 🔵 GET
+
+#     Usado para buscar dados
+#     Não deve alterar nada no servidor
+#     Parâmetros vão na URL
+#         Exemplo:
+#             GET /api/parametro?nome=Camila
+
+# 🔴 POST
+
+#     Usado para enviar dados
+#     Pode criar ou modificar recursos
+#     Dados vão no corpo (body)
+#         Exemplo:
+#             POST /api/payload
+#             {
+#             "nome": "Camila"
+#             }
