5dd620cee08b02ee719b281315820086df3daa3c
CamilaRaimundo <camilaapereira.r@gmail.com>
2026-02-17 16:28:44 -0300

    Exercício 7 - Proxy de API

--- Ex7.py
+++ Ex7.py
@@ -2,18 +2,32 @@
 # Que consuma: https://viacep.com.br/ws/{cep}/json/
 # E retorne apenas: { "logradouro": "...", "bairro": "...", "cidade": "...", "estado": "..." }
 
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 import requests
 
 app = Flask(__name__)
 
 @app.route('/cep/<string:cep>', methods=['GET'])
-def consomeAPI(cep):
+def buscar_cep(cep):
     url = f'https://viacep.com.br/ws/{cep}/json/'
     response = requests.get(url)
 
-    return response.json() if response.status_code == 200 else None 
+    if response.status_code != 200:
+        return jsonify({"erro": "Erro ao consultar API externa"}), 500
 
+    dados = response.json()
+
+    if "erro" in dados:
+        return jsonify({"erro": "CEP não encontrado"}), 404
+
+    resultado = {
+        "logradouro": dados.get("logradouro"),
+        "bairro": dados.get("bairro"),
+        "cidade": dados.get("localidade"),
+        "estado": dados.get("uf")
+    }
+
+    return jsonify(resultado), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
